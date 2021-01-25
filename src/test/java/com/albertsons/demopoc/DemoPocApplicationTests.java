bcb21fb3948f41b9f38443f1ce6903b62e7d109c
Kottamparambil R Anish <koanish@BLR.ALLEGISINDIA.COM>
2021-01-25 20:15:30 +0530

    Initial commit

---
+++ src/test/java/com/albertsons/demopoc/DemoPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertsons.demopoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
