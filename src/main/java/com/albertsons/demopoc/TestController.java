a8f0326ac3bf531cf9673baf870c397b6f0619ce
Kottamparambil R Anish <koanish@BLR.ALLEGISINDIA.COM>
2021-01-25 21:48:10 +0530

    Second commit

---
+++ src/main/java/com/albertsons/demopoc/TestController.java
@@ -0,0 +1,14 @@
+package com.albertsons.demopoc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
